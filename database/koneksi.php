4924304cc4bfba3885b5bc19e07b9ecfe4f08b31
unknown <Lathifatulalmaki@gmail.com>
2025-06-23 15:50:16 +0700

    update .env

--- database/koneksi.php
+++ database/koneksi.php
@@ -1,3 +1,20 @@
-<?php 
+<?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'sendal_jimi');
+require_once __DIR__ . '/vendor/autoload.php'; // pastikan path ke autoload.php benar
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Ambil dari .env
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$name = $_ENV['DB_NAME'];
+
+// Koneksi ke database
+$conn = mysqli_connect($host, $user, $pass, $name);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
